cc5d2e6b769dfe588aacf28f21fd10130db27850
dogbutcat <dogbutcat@hotmail.com>
2021-09-15 11:48:38 +0800

    script: fix ipv4 address fetch

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ip=$(wget -qO- -t1 -T2 ip.sb)
+ip=$(wget -qO- -t1 -T2 -4 ip.sb)
 
 args="-r ${R_IP:-$ip}:$R_PORT"
 bin="$1"
@@ -15,7 +15,12 @@ fi
 
 $bin $extArgs $args 2>&1 &
 
+exit_proc(){
+    kill -SIGTERM $(ps -e|grep sleep|grep -v grep|awk '{print $1}')
+}
+
 finish(){
+    exit_proc
     exit 0
 }
 
